237d6358861480dcb9257078e823f919312e27c9
GianM0027 <gianmario0027@gmail.com>
2024-02-22 11:33:12 +0100

    Semi-official Berts

--- models/bertFull.py
+++ models/bertFull.py
@@ -1,18 +1,27 @@
+from typing import List
+
 import torch
 from DrTorch.modules import TrainableModule
 
 
 class BertFull(TrainableModule):
 
-    def __init__(self, bert_model, bert_tokenizer, hidden_size=768, n_emotions=7):
+    def __init__(self, bert_model, hidden_size=768, n_emotions=7):
         super().__init__()
 
         self.bert = bert_model
-        self.tokenizer = bert_tokenizer
+        self.emotion_classifier = torch.nn.Linear(hidden_size, n_emotions)
+        self.trigger_classifier = torch.nn.Linear(hidden_size, 2)
 
-        self.emotions_classification_head = torch.nn.Linear(hidden_size, n_emotions)
-        self.triggers_classification_head = torch.nn.Linear(hidden_size, 2)
+    def forward(self, input_ids, cls_positions) -> List[torch.Tensor]:
+        outputs = self.bert(input_ids=input_ids)
+        sequence_output = outputs.last_hidden_state
 
-    def forward(self, kwards: dict):
-        # output come dizionario con chiavi "emotion" e "trigger"
-        pass
+        # Extracting [CLS] vectors for every sentence by using their index
+        cls_outputs = torch.stack([sequence_output[0, pos, :] for pos in cls_positions])
+
+        emotion_logits = self.emotion_classifier(cls_outputs)
+
+        trigger_logits = self.trigger_classifier(cls_outputs)
+
+        return [emotion_logits, trigger_logits]

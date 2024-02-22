9605af9660f42050c57a340ee2f645c3b37c5119
GianM0027 <gianmario0027@gmail.com>
2024-02-22 17:03:35 +0100

    Push sbagliato

--- models/bertFreezed.py
+++ models/bertFreezed.py
@@ -3,6 +3,7 @@ from typing import List
 import torch
 from DrTorch.modules import TrainableModule
 
+
 class BertFreezed(TrainableModule):
 
     def __init__(self, bert_model, hidden_size=768, n_emotions=7):
@@ -17,16 +18,22 @@ class BertFreezed(TrainableModule):
         self.emotion_classifier = torch.nn.Linear(hidden_size, n_emotions)
         self.trigger_classifier = torch.nn.Linear(hidden_size, 2)
 
-    def forward(self, input_ids, cls_positions) -> List[torch.Tensor]:
-
+    def forward(self, input_ids, sep_positions) -> List[torch.Tensor]:
         outputs = self.bert(input_ids=input_ids)
         sequence_output = outputs.last_hidden_state
 
-        # Extracting [CLS] vectors for every sentence by using their index
-        cls_outputs = torch.stack([sequence_output[0, pos, :] for pos in cls_positions])
+        # Initialize a list to hold SEP output vectors for each example in the batch
+        sep_outputs = []
 
-        emotion_logits = self.emotion_classifier(cls_outputs)
+        # Iterate over the batch and extract SEP vectors for each example
+        for i, pos in enumerate(sep_positions):
+            sep_output = sequence_output[i, pos, :]
+            sep_outputs.append(sep_output)
 
-        trigger_logits = self.trigger_classifier(cls_outputs)
+        # Stack the SEP vectors along a new dimension
+        sep_outputs = torch.stack(sep_outputs)
+
+        emotion_logits = self.emotion_classifier(sep_outputs)
+        trigger_logits = self.trigger_classifier(sep_outputs)
 
         return [emotion_logits, trigger_logits]

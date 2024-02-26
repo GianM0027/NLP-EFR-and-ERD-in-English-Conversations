8096cec52d7d35a96b306b976828c53f2e87a29d
GianM0027 <gianmario0027@gmail.com>
2024-02-26 22:31:09 +0100

    dataloaders

--- models/bertFreezed.py
+++ models/bertFreezed.py
@@ -1,24 +1,14 @@
+import math
 from typing import List, Dict
 
 import torch
 from torch import Tensor
+import numpy as np
 
 from DrTorch.modules import TrainableModule
 
 
 class BertFreezed(TrainableModule):
-
-    def __split_utterances_features(self, features, mask):
-        result = []
-        for sentence, sentence_mask in zip(features, mask):
-            sep_indices = torch.nonzero(sentence_mask, as_tuple=False).squeeze()
-            splits = []
-            for i in range(len(sep_indices) - 1):
-                start, end = sep_indices[i] + 1, sep_indices[i + 1]
-                if start < end:
-                    splits.append(sentence[start:end])
-            result.append(splits)
-        return result
 
     def __init__(self, bert_model, hidden_size=768, n_emotions=7):
         super().__init__()
@@ -34,24 +24,38 @@ class BertFreezed(TrainableModule):
         self.softmax = torch.nn.functional.softmax
         self.sigmoid = torch.nn.functional.sigmoid
 
+    def __get_n_chunk(self, input_shape):
+        batch_n, n_sentence, n_token = input_shape
+        x = math.ceil((n_sentence * n_token) / self.bert.config.max_position_embeddings)
+        while n_sentence % x != 0:
+            x += 1
+
+        return x
+
+    @staticmethod
+    def __chunk_input(n_chunk, inputs):
+        batch_n = inputs['input_ids'].shape[0]
+
+        for key, data in inputs.items():
+            inputs[key] = data.view((batch_n * n_chunk, -1))
+
+        return inputs
+
+    def __reshape(self, input, shape):
+        batch_n, n_sentence, n_token = shape
+        return input.view((batch_n, n_sentence, -1))
+
+
     def forward(self, inputs: dict) -> dict[str, Tensor]:
-        separator_mask = inputs.pop('t_sep_index')  # n_token
-        outputs = self.bert(**inputs)  # torch.Size([32, n_max_frasi, x, 768])     se paddi i dialoghi allo stesso numero di frasi x è uguale per tutte le frasi altrimenti e variabile
-        sequence_features = outputs.last_hidden_state  # torch.Size([32, 3, 31, 768])
-        print(sequence_features.shape)
+        n_chunk = self.__get_n_chunk(inputs['input_ids'].shape)
+        chunked_input = self.__chunk_input(n_chunk, inputs.copy())
+        features = self.bert(**chunked_input).last_hidden_state #todo indagare last hidden_state
+        reshaped_features = self.__reshape(features, inputs['input_ids'].shape)
+        porcodio = outputs.last_hidden_state.view((inputs['input_ids'].shape[0], inputs['input_ids'].shape[1], -1))
+        print(porcodio.shape)
+        # sequence_features = outputs.last_hidden_state
 
-        splitted_features = self.__split_utterances_features(features=sequence_features, mask=separator_mask)
+        # emotion_logits = self.emotion_classifier(sequence_features)
+        # trigger_logits = self.trigger_classifier(splitted_features)
 
-        print(len(splitted_features))  # lista di list di tensori
-
-        for l in splitted_features:
-            print(len(l))
-            print((l[0].shape))
-            print((l[1].shape))
-            print((l[2].shape))
-
-        emotion_logits = self.emotion_classifier(splitted_features)  # B, N_frasi, C
-
-        trigger_logits = self.trigger_classifier(splitted_features)  # B, N_frasi, 1
-
-        return {'emotions': self.softmax(emotion_logits), 'triggers': self.sigmoid(trigger_logits)}
+        return 0  # {'emotions': self.softmax(emotion_logits, dim=1), 'triggers': self.sigmoid(trigger_logits)}

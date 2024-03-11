1039bedb388095d40fcf53f3162b3b2da3d46796
Riccardo Murgia <64796293+RiccardoMurgia@users.noreply.github.com>
2024-03-11 22:08:47 +0100

    Introduce the Licence in DrTorch and reorganized the notebook

--- DrTorch/metrics.py
+++ DrTorch/metrics.py
@@ -2,18 +2,40 @@
 
 
 
- /$$$$$$$         /$$$$$$$$                            /$$      
-| $$__  $$       |__  $$__/                           | $$      
-| $$  \ $$  /$$$$$$ | $$  /$$$$$$   /$$$$$$   /$$$$$$$| $$$$$$$ 
+ /$$$$$$$         /$$$$$$$$                            /$$
+| $$__  $$       |__  $$__/                           | $$
+| $$  \ $$  /$$$$$$ | $$  /$$$$$$   /$$$$$$   /$$$$$$$| $$$$$$$
 | $$  | $$ /$$__  $$| $$ /$$__  $$ /$$__  $$ /$$_____/| $$__  $$
 | $$  | $$| $$  \__/| $$| $$  \ $$| $$  \__/| $$      | $$  \ $$
 | $$  | $$| $$      | $$| $$  | $$| $$      | $$      | $$  | $$
 | $$$$$$$/| $$      | $$|  $$$$$$/| $$      |  $$$$$$$| $$  | $$
 |_______/ |__/      |__/ \______/ |__/       \_______/|__/  |__/
-                                                                
+
+
+
+# ----------------------------------------------------------------------------#
+# Copyright(C) 2023-2024 Davide Femia and Riccardo Murgia. Italy              #
+#                                                                             #
+#                                                                             #
+# All rights reserved.                                                        #
+#                                                                             #
+# This software is the property of Davide Femia and Riccardo Murgia           #
+# and may not be modified, distributed, or used without explicit permission.  #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+# File:     __init__.py                                                       #
+# Authors:  Davide Femia     <femiadavide04@gmail.com>                        #
+#           Riccardo Murgia  <murgiariccardo96@gmail.com>                     #
+#                                                                             #
+# Date:      11.03.2024                                                       #
+#                                                                             #
+# ----------------------------------------------------------------------------#
+
 
 
 """
+
 
 from typing import Optional, Callable, List, Any, Dict, Tuple
 from sympy import Float
--- DrTorch/model_selection.py
+++ DrTorch/model_selection.py
@@ -13,7 +13,29 @@
 
 
 
+# ----------------------------------------------------------------------------#
+# Copyright(C) 2023-2024 Davide Femia and Riccardo Murgia. Italy              #
+#                                                                             #
+#                                                                             #
+# All rights reserved.                                                        #
+#                                                                             #
+# This software is the property of Davide Femia and Riccardo Murgia           #
+# and may not be modified, distributed, or used without explicit permission.  #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+# File:     __init__.py                                                       #
+# Authors:  Davide Femia     <femiadavide04@gmail.com>                        #
+#           Riccardo Murgia  <murgiariccardo96@gmail.com>                     #
+#                                                                             #
+# Date:      11.03.2024                                                       #
+#                                                                             #
+# ----------------------------------------------------------------------------#
+
+
+
 """
+
 
 import inspect
 
